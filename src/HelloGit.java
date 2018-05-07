c87db89495f6b69221e2864323f5975d20d40917
garci <garci@HP-Erik>
2018-05-07 18:54:49 +0200

    Este es la segunda versión, primera en GitHub

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,6 +3,8 @@ public class HelloGit {
 
 	public static void main(String[] args){
 		System.out.println("Hello Git");
+		System.out.println("This is a little programm");
+		System.out.println("Tgfhfdg");
 	}
 }
 	
